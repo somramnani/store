af1c3789478bfebf615a10e9c5dcce448a6a506e
somramnani <som.ramnani@gmail.com>
2024-08-28 12:13:12 -0400

    Add quantity in cart

--- src/components/ItemCart.tsx
+++ src/components/ItemCart.tsx
@@ -1,4 +1,3 @@
-import useShoppingCart from "../hooks/useShoppingCart";
 import { Stack } from "react-bootstrap";
 import storeItems from "../data/items.json";
 
@@ -8,16 +7,25 @@ type ItemCartProps = {
 };
 
 const ItemCart = ({ id, quantity }: ItemCartProps) => {
-  const { removeFromCart } = useShoppingCart();
   const item = storeItems.find((i) => i.id === id);
   if (item === null) return null;
 
   return (
-    <Stack direction="horizontal" gap={2} className="d-flex">
+    <Stack direction="horizontal" gap={2} className="d-flex align-items-center">
       <img
         src={item?.imgUrl}
         style={{ width: "124px", height: "75px", objectFit: "cover" }}
       />
+      <div className="me-auto">
+        <div>
+          {item?.name}
+          {quantity > 1 && (
+            <span className="text-muted" style={{ fontSize: ".65rem" }}>
+              {quantity} x
+            </span>
+          )}
+        </div>
+      </div>
     </Stack>
   );
 };
