ce392018f0aef2c5b17e7a421d5d8f099c634e01
somramnani <som.ramnani@gmail.com>
2024-08-28 12:46:31 -0400

    Add homepage hero image and text

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,22 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="hero">
+      <div className="overlay">
+        <h1 className="display-3">Welcome to Our Store</h1>
+        <p className="lead">Find the best products at unbeatable prices</p>
+        <Link to="/store">
+          <Button variant="primary" size="lg">
+            Shop Now
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
