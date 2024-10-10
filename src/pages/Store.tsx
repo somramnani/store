9dc9f47ee251dcf020d9007323ec7dbb189e9a86
somramnani <som.ramnani@gmail.com>
2024-10-10 17:04:44 -0400

    Remove h1 titles in Store and About page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -5,8 +5,6 @@ import StoreItem from "../components/StoreItem";
 const Store = () => {
   return (
     <div>
-      <h1>Store</h1>
-
       <Row className="g-3" md={2} xs={1} lg={3}>
         {storeItems.map((item) => {
           return (
